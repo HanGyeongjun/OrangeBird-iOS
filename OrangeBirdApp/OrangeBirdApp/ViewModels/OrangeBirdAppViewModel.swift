6f183c774f0bb33beec867bb173972b170b4c112
HanGyeongjun <030212hgjun@gmail.com>
2023-11-14 21:23:16 +0900

    [Init] #2 - Set Directory Structure

---
+++ OrangeBirdApp/OrangeBirdApp/ViewModels/OrangeBirdAppViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  OrangeBirdAppViewModel.swift
+//  OrangeBirdApp
+//
+//  Created by HanGyeongjun on 11/14/23.
+//
+
+import Foundation
---
+++ OrangeBirdApp/OrangeBirdApp/Views/Components/CallToActionButton.swift
@@ -0,0 +1,18 @@
+//
+//  CallToActionButton.swift
+//  OrangeBirdApp
+//
+//  Created by HanGyeongjun on 11/14/23.
+//
+
+import SwiftUI
+
+struct CallToActionButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CallToActionButton()
+}
