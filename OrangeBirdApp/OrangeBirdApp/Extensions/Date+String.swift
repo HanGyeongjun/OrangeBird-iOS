18e4debebbb8ca415315ead71661e57a317e7081
HanGyeongjun <030212hgjun@gmail.com>
2023-11-15 21:48:39 +0900

    [Feat] #7 - Create Date+String Extension

---
+++ OrangeBirdApp/OrangeBirdApp/Extensions/Date+String.swift
@@ -0,0 +1,23 @@
+//
+//  Date+String.swift
+//  OrangeBirdApp
+//
+//  Created by HanGyeongjun on 11/15/23.
+//
+
+import Foundation
+
+extension Date {
+    
+    /// Date를 yyyyMMddHHmmss 형태의 String으로 변환해주는 함수입니다.
+    /// 다른 형식의 Date가 필요하다면 속성을 추가하면 됩니다.
+    func getDate() -> String {
+        let current = Date()
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ko_kr")
+        formatter.timeZone = TimeZone(abbreviation: "KST")
+        formatter.dateFormat = "yyyyMMddHHmmss"
+        
+        return formatter.string(from: current)
+    }
+}
